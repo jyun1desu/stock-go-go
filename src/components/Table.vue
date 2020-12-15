844545042a501cd310751fd07aeace32a6bc2236
jyun1 <jyuny12345@gmail.com>
2020-12-15 11:17:31 +0800

    get rows title data

---
+++ src/components/Table.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="table">
+    <h1>of</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Table",
+  data() {
+    return {
+      rows:[],
+    };
+  },
+  mounted() {
+    fetch("https://5fbd1e2b3f8f90001638cc76.mockapi.io/layer")
+      .then((res) => res.json())
+      .then((rowTitles) => {
+        this.rows = rowTitles;
+      });
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home">  
+    <Table/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Table from '@/components/Table.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Table
   }
 }
 </script>
+<style lang="scss" scoped>
+</style>
